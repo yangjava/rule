cbe78dcb2e91840f8c5192ec0a7ee67fd6f4919f
yangjingjing <qjd123456>
2019-08-01 09:05:50 +0800

    add

---
+++ src/main/java/com/rule/entity/AddressCheckResult.java
@@ -0,0 +1,14 @@
+package com.rule.entity;
+
+public class AddressCheckResult {
+
+    private boolean postCodeResult = false;
+
+    public boolean isPostCodeResult() {
+        return postCodeResult;
+    }
+
+    public void setPostCodeResult(boolean postCodeResult) {
+        this.postCodeResult = postCodeResult;
+    }
+}
